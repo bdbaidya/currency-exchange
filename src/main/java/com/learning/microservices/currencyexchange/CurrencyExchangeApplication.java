c74203f8a181e0f4dfd62fb2c7c48e0bd49753f9
Debashis B <debashis.baidya@kiwigrid.com>
2024-04-05 03:49:55 +0200

    Zipkin enabled

--- src/main/java/com/learning/microservices/currencyexchange/CurrencyExchangeApplication.java
+++ src/main/java/com/learning/microservices/currencyexchange/CurrencyExchangeApplication.java
@@ -2,8 +2,10 @@ package com.learning.microservices.currencyexchange;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zipkin2.server.internal.EnableZipkinServer;
 
 @SpringBootApplication
+@EnableZipkinServer
 public class CurrencyExchangeApplication {
 
 	public static void main(String[] args) {
